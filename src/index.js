532175e5db4d0e011ecbaf313e21fafa9d187420
Wilk <wilkrosa1998@gmail.com>
2022-07-27 16:27:40 -0400

    finalized

--- src/index.js
+++ src/index.js
@@ -79,4 +79,40 @@ app.post('/withdraw', verifyIfExistsAccountCPF, (req, res) => {
   customer.statement.push(statementOperation);
   return res.status(201).send();
 });
+app.get('/statement/date', verifyIfExistsAccountCPF, (req, res) => {
+  const { customer } = req;
+  const { date } = req.query;
+
+  const dateFormat = new Date(date + ' 00:00');
+  const statement = customer.statement.filter(
+    statement =>
+      statement.createdAt.toDateString() === new Date(dateFormat).toDateString()
+  );
+  return res.json(statement);
+});
+app.put('/account', verifyIfExistsAccountCPF, (req, res) => {
+  const { name } = req.body;
+  const { customer } = req;
+  customer.name = name;
+
+  return res.status(201).send();
+});
+app.get('/account', verifyIfExistsAccountCPF, (req, res) => {
+  const { customer } = req;
+
+  return res.json(customer);
+});
+app.delete('/account', verifyIfExistsAccountCPF, (req, res) => {
+  const { customer } = req;
+  customers.splice(customer, 1);
+
+  return res.status(200).send(customers);
+});
+app.get('/balance', verifyIfExistsAccountCPF, (req, res) => {
+  const { customer } = req;
+
+  const balance = getBalance(customer.statement);
+
+  return res.json(balance);
+});
 app.listen(3333);
